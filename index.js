185be14c18803ea3f63e7e9c4026db3d9aeddeed
Siraj Alam <sirajalam@Sirajs-MacBook-Pro.local>
2020-02-14 20:29:41 +0530

    Url is added in the parameter

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const ssr = require('./ssr.js');
 const app = express();
 
 app.get('/*', async (req, res, next) => {
-    const { protocol, host, path } = req;
-    const url = `${protocol}://${host}${path}`;
-    // const url = `https://beta.garbagevalue.com${req['path']}`;
+    const { query } = req;
+    const url = query['url'];
+    if (!url) return res.status(200);
     const response = await ssr(url);
     // console.log('Res', response);
     // Add Server-Timing! See https://w3c.github.io/server-timing/.
